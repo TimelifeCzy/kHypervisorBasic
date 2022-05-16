e8d35e7943d3f1d0eaea0b8fc73b0d92a46e9b4a
酒贩子 <43507143+TimelifeCzy@users.noreply.github.com>
2022-05-16 17:58:08 +0800

    Basic code supplement

--- README.md
+++ README.md
@@ -3,11 +3,16 @@ kHypervisor: https://github.com/Kelvinhack/kHypervisor
 ### v1.0
 基于kHypervisor框架添加了Msr/Ept Hook，仅用于学习和示例使用。
 
+**see directory: kHypervisor/kHypervisor**
+
 ### v2.0
 khypervisor分離，設計了一套hv_lib連接接口
+```
 |-- hv 
   |-- nested hv
   |-- pci device mon
   |-- ept
   	|-- ept hook
 ...
+```
+**see directory: kHypervisor/kHypervisorBasic**
---
+++ kHypervisorBasic/vmx.h
@@ -0,0 +1,111 @@
+/*++
+
+Copyright (c) 2016 KelvinChan. All rights reserved.
+Use of this source code is governed by a MIT-style license that can be
+found in the LICENSE file.
+
+Module Name:
+
+	vmx.h
+
+Abstract:
+
+	VMX Instruction Emulation
+
+Author:
+	
+	Kelvin Chan
+
+Environment:
+
+	Kernel VMM Mode
+
+--*/
+#pragma once
+#ifndef NESTED_HYPERPLATFORM_VMX_H_
+#define NESTED_HYPERPLATFORM_VMX_H_
+#include <fltKernel.h>
+#include "vmm.h"
+#include "util.h"
+
+struct GuestContext;
+
+enum VMX_state
+{
+	VMCS_STATE_CLEAR = 0,
+	VMCS_STATE_LAUNCHED
+};
+
+extern "C"
+{
+
+VOID 
+VmxVmxonEmulate(
+	_In_ GuestContext* guest_context
+);
+
+VOID 
+VmxVmxoffEmulate(
+	_In_ GuestContext* guest_context
+);
+
+VOID 
+VmxVmclearEmulate(
+	_In_ GuestContext* guest_context
+);
+
+VOID 
+VmxVmptrldEmulate(
+	_In_ GuestContext* guest_context
+);
+
+VOID 
+VmxVmreadEmulate(
+	_In_ GuestContext* guest_context
+);
+
+VOID 
+VmxVmwriteEmulate(
+	_In_ GuestContext* guest_context
+);
+
+VOID 
+VmxVmlaunchEmulate(
+	_In_ GuestContext* guest_context
+);
+
+VOID 
+VmxVmresumeEmulate(
+	_In_ GuestContext* guest_context
+);
+
+VOID 
+VmxVmptrstEmulate(
+	_In_ GuestContext* guest_context
+);
+
+VOID VmxInveptEmulate(
+	_In_ GuestContext* guest_context
+);
+VOID 
+LEAVE_GUEST_MODE(
+	_In_ VCPUVMX* vcpu
+);
+VOID 
+ENTER_GUEST_MODE(
+	_In_ VCPUVMX* vcpu
+);
+VMX_MODE
+VmxGetVmxMode(
+	_In_ VCPUVMX* vcpu
+);  
+
+NTSTATUS 
+VmxVMExitEmulate(
+	_In_ VCPUVMX* vCPU,
+	_In_ GuestContext* guest_context
+);
+
+}
+
+#endif
